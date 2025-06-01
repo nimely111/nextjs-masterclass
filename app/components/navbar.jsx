49dc1e19e35068588394d1d5ec34516072b7447f
Nimely111 <samuelehnimely@gmail.com>
2025-06-01 02:18:02 +0000

    update project

---
+++ app/components/navbar.jsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+const Navbar = () => {
+  return (
+    <nav>
+      <h1>Dojo HelpDesk</h1>
+      <Link href="/">Dashboard</Link>
+      <Link href="/tickets">Tickets</Link>
+    </nav>
+  );
+};
+
+export default Navbar;
